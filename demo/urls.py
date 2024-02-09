bf210f6bfd419c12cee6f93779ce5522e67ce2e4
hamonangann <bornyto.hamonangan@ui.ac.id>
2024-02-09 22:54:45 +0700

    add form and db migration

--- demo/urls.py
+++ demo/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from home.views import homepage
+from home.views import homepage, welcomepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', homepage)
+    path('', welcomepage),
+    path('home/', homepage),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Visitor
+from .forms import NewVisitorForm
+
 
 # Create your views here.
+def welcomepage(request):
+    if request.method == 'POST':
+        form = NewVisitorForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+        else:
+            messages.error(request, 'Sorry, failed to save your name.')
+        
+        return redirect('/home')
+    
+    if request.method == 'GET':
+        form = NewVisitorForm()
+        return render(request, 'welcome.html', {'form': form})
+
+
 def homepage(request):
-    return render(request, 'index.html', {})
+    visitors = Visitor.objects.all()
+    return render(request, 'home.html', {'visitors': visitors})
